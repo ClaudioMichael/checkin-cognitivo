adb897224d6cc7280f873aa993473a33df4c33cb
Claudio Michael <79266544+ClaudioMichael@users.noreply.github.com>
2026-02-18 19:24:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,54 +12,54 @@ nome = st.text_input("Digite seu nome ou matrícula")
 
 # estados
 if "rodada" not in st.session_state:
-st.session_state.rodada = 0
-st.session_state.tempos = []
-st.session_state.mostrar_botao = False
-st.session_state.inicio = 0
+    st.session_state.rodada = 0
+    st.session_state.tempos = []
+    st.session_state.mostrar_botao = False
+    st.session_state.inicio = 0
 
 # iniciar teste
 if st.button("Iniciar Teste"):
-st.session_state.rodada = 1
-st.session_state.tempos = []
-st.session_state.mostrar_botao = False
+    st.session_state.rodada = 1
+    st.session_state.tempos = []
+    st.session_state.mostrar_botao = False
 
 # rodadas
 if st.session_state.rodada >= 1 and st.session_state.rodada <= 5:
 
-st.write(f"Rodada {st.session_state.rodada}/5")
+    st.write(f"Rodada {st.session_state.rodada}/5")
 
 if not st.session_state.mostrar_botao:
-st.info("Aguarde aparecer o botão...")
-delay = random.uniform(2,5)
-time.sleep(delay)
-st.session_state.mostrar_botao = True
-st.session_state.inicio = time.time()
-st.rerun()
+    st.info("Aguarde aparecer o botão...")
+    delay = random.uniform(2,5)
+    time.sleep(delay)
+    st.session_state.mostrar_botao = True
+    st.session_state.inicio = time.time()
+    st.rerun()
 
 if st.session_state.mostrar_botao:
-if st.button("CLIQUE AGORA!"):
-tempo = (time.time() - st.session_state.inicio) * 1000
-st.session_state.tempos.append(tempo)
-st.success(f"Tempo: {int(tempo)} ms")
+    if st.button("CLIQUE AGORA!"):
+        tempo = (time.time() - st.session_state.inicio) * 1000
+        st.session_state.tempos.append(tempo)
+        st.success(f"Tempo: {int(tempo)} ms")
 
-st.session_state.rodada += 1
-st.session_state.mostrar_botao = False
-st.rerun()
+        st.session_state.rodada += 1
+        st.session_state.mostrar_botao = False
+        st.rerun()
 
 # resultado final
 if st.session_state.rodada > 5:
 
-tempos = st.session_state.tempos
-media = sum(tempos)/len(tempos)
-desvio = pd.Series(tempos).std()
+    tempos = st.session_state.tempos
+    media = sum(tempos)/len(tempos)
+    desvio = pd.Series(tempos).std()
 
-st.subheader("Resultado")
-st.write(f"Tempo médio: {int(media)} ms")
-st.write(f"Variabilidade: {int(desvio)}")
+    st.subheader("Resultado")
+    st.write(f"Tempo médio: {int(media)} ms")
+    st.write(f"Variabilidade: {int(desvio)}")
 
-if media <= 280 and desvio <= 40:
-st.success("🟢 PRONTIDÃO IDEAL")
-elif media <= 350 and desvio <= 70:
-st.warning("🟡 ATENÇÃO")
-else:
-st.error("🔴 RISCO")
+    if media <= 280 and desvio <= 40:
+        st.success("🟢 PRONTIDÃO IDEAL")
+    elif media <= 350 and desvio <= 70:
+        st.warning("🟡 ATENÇÃO")
+    else:
+        st.error("🔴 RISCO")
